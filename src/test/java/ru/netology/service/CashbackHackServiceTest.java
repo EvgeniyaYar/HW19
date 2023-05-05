3d7ae103a769cf1a0e534f3c1e1826e549ef170d
Evgeniya Yarotskaya <evgeniya047@mail.ru>
2023-05-04 19:47:19 -0700

    Junit4 version

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,33 +1,32 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 public class CashbackHackServiceTest {
-    @Test
+    @org.junit.Test
     public void shouldCalculateAmountIfLess1000() {
         CashbackHackService service = new CashbackHackService();
         int expected = 100;
         int actual = service.remain(900);
-        Assertions.assertEquals(expected, actual);
+        Assert.assertEquals(expected, actual);
     }
 
-    @Test
+    @org.junit.Test
     public void shouldCalculateAmountIf1000() {
         CashbackHackService service = new CashbackHackService();
         int expected = 0;
         int actual = service.remain(1000);
-        Assertions.assertEquals(expected, actual);
+        Assert.assertEquals(expected, actual);
     }
 
-    @Test
+    @org.junit.Test
     public void shouldCalculateAmountIfMore1000() {
         CashbackHackService service = new CashbackHackService();
         int expected = 300;
         int actual = service.remain(3700);
-        Assertions.assertEquals(expected, actual);
+        Assert.assertEquals(expected, actual);
     }
 
 }
